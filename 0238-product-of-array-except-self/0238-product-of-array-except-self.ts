2f81ac802192c068b6432172f7b062742e943ddd
Eva <60597453+Evalin123@users.noreply.github.com>
2024-12-23 19:43:47 +0800

    Time: 2 ms (97.22%), Space: 63.8 MB (68.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,19 +1,33 @@
 function productExceptSelf(nums: number[]): number[] {
-    let prefix = [1]; 
-    let suffix = [1]; 
-    let ans = [];
+   let ans = new Array(nums.length).fill(1);
 
-    for (let i = 0; i < nums.length; i++){
-        prefix.push(prefix[i] * nums[i]);
+    let prefix = 1;
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = prefix;
+        prefix *= nums[i];
     }
 
-    for (let j = nums.length - 1; j > 0; j--) {
-        suffix.unshift(suffix[0] * nums[j]);
+    let suffix = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        ans[i] *= suffix;
+        suffix *= nums[i];
     }
 
-    for (let i = 0; i < nums.length; i++){
-        ans.push(prefix[i] * suffix[i]);
-    }
+    // let prefix = [1]; 
+    // let suffix = [1]; 
+    // let ans = [];
+
+    // for (let i = 0; i < nums.length; i++){
+    //     prefix.push(prefix[i] * nums[i]);
+    // }
+
+    // for (let j = nums.length - 1; j > 0; j--) {
+    //     suffix.unshift(suffix[0] * nums[j]);
+    // }
+
+    // for (let i = 0; i < nums.length; i++){
+    //     ans.push(prefix[i] * suffix[i]);
+    // }
 
     return ans
 };
