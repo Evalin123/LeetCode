7c589155c5e2d5fdeec4143c298f1160c9314723
Eva <60597453+Evalin123@users.noreply.github.com>
2025-01-14 14:38:02 +0800

    Time: 2 ms (100%), Space: 56.5 MB (19.15%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.ts
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.ts
@@ -1,17 +1,35 @@
 function numSubarrayProductLessThanK(nums: number[], k: number): number {
-    let res = 0;
+    if (k <= 1) return 0; // Handle edge case where no product can be less than k
 
-    for (let i = 0; i < nums.length; i++) {
-        let prod = 1;
-        for (let j = i; j < nums.length; j++) {
-            prod = prod * nums[j];
-            if (prod >= k) {
-                break;
-            } else {
-                res++;
-            }
+    let count = 0;
+    let product = 1;
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        product *= nums[right];
+
+        while (product >= k) {
+            product /= nums[left];
+            left++;
         }
+
+        count += right - left + 1;
     }
 
-    return res
+    return count;
+    // let res = 0;
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     let prod = 1;
+    //     for (let j = i; j < nums.length; j++) {
+    //         prod = prod * nums[j];
+    //         if (prod >= k) {
+    //             break;
+    //         } else {
+    //             res++;
+    //         }
+    //     }
+    // }
+
+    // return res
 };
