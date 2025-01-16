5d05149a417f7946d1b5c7c090f6b05447291740
Eva <60597453+Evalin123@users.noreply.github.com>
2025-01-16 17:26:04 +0800

    Time: 0 ms (100%), Space: 53 MB (41.78%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.ts
+++ 0724-find-pivot-index/0724-find-pivot-index.ts
@@ -1,15 +1,4 @@
 function pivotIndex(nums: number[]): number {
-    // let leftSum = [0];
-    // let rightSum = [0];
-
-    // for(let i = 0; i < nums.length; i++) {
-    //     leftSum.push(leftSum[i] + nums[i])
-    // }
-
-    // for(let j = nums.length - 1; j >= 0; j--) {
-    //     rightSum.unshift(rightSum[nums.length - 1 - j])
-    // }
-
     let sumLeft = 0;
     let total = nums.reduce((a,b)=>a+b);
     for(let i = 0; i < nums.length; i++) {
