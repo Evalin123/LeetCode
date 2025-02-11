d5aae93187c1502b93812ee0c87d1c1b043b0fc5
Eva <60597453+Evalin123@users.noreply.github.com>
2025-02-11 12:24:59 +0800

    Time: 12 ms (95.96%), Space: 52 MB (82.51%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.ts
+++ 0016-3sum-closest/0016-3sum-closest.ts
@@ -2,24 +2,22 @@ function threeSumClosest(nums: number[], target: number): number {
     nums.sort((a, b) => a - b);
 
     let closest = nums[0] + nums[1] + nums[2];
-    for (let i = 0; i < nums.length - 2; i++) {
+    for (let i = 0; i < nums.length; i++) {
         let left = i + 1;
         let right = nums.length - 1;
 
-        while (left < right) {
+        while(left < right) {
             let sum = nums[i] + nums[left] + nums[right];
-
-            
-            if (sum === target) return sum;
-
-            if (Math.abs(sum - target) < Math.abs(closest - target)) {
+            if (sum === target) {
+                return sum;
+            }  
+            if (Math.abs(target - sum) < Math.abs(target - closest)) {
                 closest = sum;
             }
-
             if (sum < target) {
-                left++;  
+                left ++
             } else {
-                right--; 
+                right --
             }
         }
     }
