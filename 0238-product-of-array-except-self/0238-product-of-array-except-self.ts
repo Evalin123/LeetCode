eb62d0da83b3db1d69c98088f396a9397c7ff748
Eva <60597453+Evalin123@users.noreply.github.com>
2025-11-24 17:44:00 +0800

    Time: 5 ms (79.41%), Space: 69 MB (88.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,16 +1,15 @@
 function productExceptSelf(nums: number[]): number[] {
-    const n = nums.length;
-    const output = new Array(n).fill(1);
+    const output = new Array(nums.length).fill(1);
 
     // prefix
-    for(let i = 1; i < n; i++) {
+    for(let i = 1; i < nums.length; i++) {
         output[i] = output[i - 1] * nums[i - 1]
     }
 
     // suffix
     let right = 1;
     
-    for (let i = n - 1; i >=0; i--) {
+    for (let i = nums.length - 1; i >=0; i--) {
         output[i] = right * output[i]
         right = nums[i] * right
     }
